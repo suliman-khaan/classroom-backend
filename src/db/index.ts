c2db57c273bad1963c89ad4361a5e02e3853f3bd
Suliman Khan <sulimank418@gmail.com>
2026-02-17 14:14:20 +0500

    implement GET all subjects route

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,5 +7,5 @@ if (!process.env.DATABASE_URL) {
 }
 
 const sql = neon(process.env.DATABASE_URL);
-export const index = drizzle(sql);
-export const pool = undefined;
+export const db = drizzle(sql);
+
