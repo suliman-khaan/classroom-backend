5a71ed1a795634b1f05e5e2d736fa31c7bc50e57
Suliman Khan <sulimank418@gmail.com>
2026-02-17 15:33:18 +0500

    Server file syntex error fixed

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import subjectRouter from "./routes/subjects";
 const app = express();
 const PORT = 8000;
 
-if(process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file');
+if(!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file');
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
